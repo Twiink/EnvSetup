20372e76938f1e40c259eace1336584fdf8314c3
Twiink <zy13990529155@163.com>
2026-03-23 13:58:56 +0800

    del不必要的文件及文件夹

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environmentMatchGlobs: [['tests/renderer/**', 'jsdom']],
     include: ['tests/unit/**/*.test.ts', 'tests/renderer/**/*.test.tsx'],
     exclude: ['tests/e2e/**'],
     setupFiles: ['./tests/setup.ts'],
